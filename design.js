33e02068f11c2c0d72b99298821bbd64736041ba
Vyshnavi17-11 <135938623+Vyshnavi17-11@users.noreply.github.com>
2023-07-28 16:03:15 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,16 @@
+ReactDOM.render(
+<div>
+    <img src='https://cdn.britannica.com/04/171104-050-AEFE3141/Steve-Jobs-iPhone-2010.jpg' height='250' width='250'/>
+    <p> Steven Paul Jobs was an American business magnate, inventor, and investor. He was the co-founder, chairman, and CEO of Apple; the chairman and majority shareholder of Pixar; a member of The Walt Disney Company's board of directors following its acquisition of Pixar; and the founder, chairman, and CEO of NeXT;</p>
+    <uL>
+    <li>Born: 24 February 1955, San Francisco, California, United States</li>
+    <li>Education:Reed College (attended)</li>
+    <li>Occupations:Businessmanindustrial ,designerinvestormedia ,proprietorphilanthropist</li>
+    <li>Known for:Pioneer of the personal computer revolution with Steve Wozniak,Co-creator of the Apple II, Macintosh, iPod, iPhone, iPad, and first Apple Stores</li>
+    <li>Board member of:The Walt Disney Company,Apple Inc</li>
+    <li>Awards	:Presidential Medal of Freedom (posthumous, 2022)</li>
+    <li>Died:October 5, 2011 (aged 56),Palo Alto, California, U.S.</li>
+    </uL>
+    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Steve_Jobs_signature.svg/150px-Steve_Jobs_signature.svg.png'/>
+   </div>,
+   document.getElementById('content'));
